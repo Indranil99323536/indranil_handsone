eb412e2f172dc88e05bbad97f476c3e2ccbf3d55
Indranil99323536 <talktoneildas2@gmail.com>
2021-12-08 16:38:09 +0530

    modified

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -3,7 +3,9 @@ function ShowFormValue() {
     var _lastname = document.getElementById('txt_LastName').value;
     var _swon = document.getElementById('txt_SWON').value;
     var _doj = document.getElementById('txt_DateofJoining').value;
-    var _rollno = document.getElementById('txt_Roll_NO').value;
+    var _txt_Roll_NO = document.getElementById('txt_Roll_NO').value;
     var _txt_Personsal = document.getElementById('txt_Personsal ID').value;
-    alert(_firstname + '\n\n' + _lastname + '\n\n' + _swon + '\n\n' +'\n\n' + _rollno + '\n\n' + _txt_Personsal);
+    var _txt_Pasword = document.getElementById('txt_Password').value;
+
+    alert(_firstname + '\n\n' + _lastname + '\n\n' + _swon + '\n\n' + _doj + '\n\n' + _txt_Roll_NO +'\n\n' + _txt_Personsal + '\n\n' + _txt_Pasword);
 }
