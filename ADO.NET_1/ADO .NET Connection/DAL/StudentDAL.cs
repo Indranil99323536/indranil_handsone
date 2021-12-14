bc1583a541c1194529dfbb16c6211a7801a9ee2f
Indranil99323536 <talktoneildas2@gmail.com>
2021-12-14 15:40:51 +0530

    Initial Commit for ADO.Net application

---
+++ ADO.NET_1/ADO .NET Connection/DAL/StudentDAL.cs
@@ -0,0 +1,212 @@
+﻿using ADO.NetConnection.BO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NetConnection.DAL
+{
+    public class StudentDAL
+    {
+        private readonly string _connectionString;
+        public StudentDAL()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            _connectionString = configuration.GetConnectionString("DBConnectionString");
+            //_connectionString = "Data Source=localhost;Initial Catalog=Training;Integrated Security=True;TrustServerCertificate=True";
+        }
+        public List<Student> GetStudents()
+        {
+            var studentList = new List<Student>();
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string str = "select * from Student";
+                using (SqlCommand command = new SqlCommand(str, sqlConnection))
+                {
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var student = new Student();
+                        student.FN = reader["First_Name"] as string;
+                        student.LN = reader["Last_Name"] as string;
+                        student.RollNo = reader["RollNo"] as int?;
+                        student.Marks = reader["Marks"] as double?;
+                        studentList.Add(student);
+                    }
+
+                    reader.Close();
+                    sqlConnection.Close();                    
+                }
+            }
+            return studentList;
+        }
+
+        public Student GetStudentByRollNumber(int RollNo)
+        {
+            var student = new Student();
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                string str = "select * from Student where RollNo=@RollNo";
+
+                using (SqlCommand command = new SqlCommand(str, sqlConnection))
+                {
+                    command.Parameters.AddWithValue("@RollNo", RollNo);
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+
+                        student.FN = reader["First_Name"] as string;
+                        student.LN = reader["Last_Name"] as string;
+                        student.RollNo = reader["RollNo"] as int?;
+                        student.Marks = reader["Marks"] as double?;
+                    }
+
+                    reader.Close();
+                    sqlConnection.Close();
+                }
+            }
+            return student;
+        }
+
+        public bool CreateStudent(Student student)
+        {
+            bool isSuccess = true;
+            try
+            {
+
+                using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+                {
+                    sqlConnection.Open();
+                    string str = "INSERT INTO Student (FIRST_NAME, LAST_NAME,ROLLNO,MARKS) VALUES (@First_Name,@Last_Name,@RollNo,@marks);" +
+                        "SET @id=SCOPE_IDENTITY();";
+                    int? id = 0;
+                    var sqlParamId = new SqlParameter { Direction = System.Data.ParameterDirection.Output, ParameterName = "@id", DbType = System.Data.DbType.Int32};
+                   
+
+                    using (SqlCommand command = new SqlCommand(str, sqlConnection))
+                    {
+                        command.Parameters.AddWithValue("@First_Name", student.FN);
+                        command.Parameters.AddWithValue("@Last_Name", student.LN);
+                        command.Parameters.AddWithValue("@RollNo", student.RollNo);
+                        command.Parameters.AddWithValue("@marks", student.Marks);
+                        command.Parameters.Add(sqlParamId);
+                        var result = command.ExecuteNonQuery();
+                        id = sqlParamId.Value as int?;
+                        sqlConnection.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                Console.WriteLine("An error occurred. " + e.Message);
+                throw;
+            }
+            return isSuccess;
+        }
+
+        public bool CreateStudentWithSP(Student student)
+        {
+            bool isSuccess = true;
+            try
+            {
+
+                using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+                {
+                    sqlConnection.Open();
+                    string str = "user_defined";
+                    using (SqlCommand command = new SqlCommand(str, sqlConnection))
+                    {
+                        command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@First_Name", student.FN);
+                        command.Parameters.AddWithValue("@Last_Name", student.LN);
+                        command.Parameters.AddWithValue("@RollNo", student.RollNo);
+                        command.Parameters.AddWithValue("@marks", student.Marks);
+                        var result = command.ExecuteScalar();
+                        sqlConnection.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                Console.WriteLine("An error occurred. " + e.Message);
+                throw;
+            }
+            return isSuccess;
+        }
+
+        public bool UpdateStudent(Student student)
+        {
+            bool isSuccess = true;
+            try
+            {
+
+                using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+                {
+                    sqlConnection.Open();
+                    string str = "UPDATE Student " +
+                        "SET FIRST_NAME=@First_Name, " +
+                            "LAST_NAME=@Last_Name,"+
+                            "MARKS=@marks " +
+                         "WHERE ROLLNO=@RollNo;" ;
+
+                    using (SqlCommand command = new SqlCommand(str, sqlConnection))
+                    {
+                        command.Parameters.AddWithValue("@First_Name", student.FN);
+                        command.Parameters.AddWithValue("@Last_Name", student.LN);
+                        command.Parameters.AddWithValue("@RollNo", student.RollNo);
+                        command.Parameters.AddWithValue("@marks", student.Marks);
+                        var result = command.ExecuteNonQuery();
+                        sqlConnection.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                Console.WriteLine("An error occurred. " + e.Message);
+                throw;
+            }
+            return isSuccess;
+        }
+
+        public bool DeleteStudent(int rollNumber)
+        {
+            bool isSuccess = true;
+            try
+            {
+
+                using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+                {
+                    sqlConnection.Open();
+                    string str = "DELETE Student " +Environment.NewLine +
+                         "WHERE ROLLNO=@rollNo;";
+
+                    using (SqlCommand command = new SqlCommand(str, sqlConnection))
+                    {
+                        command.Parameters.AddWithValue("@rollNo", rollNumber);
+                        var result = command.ExecuteNonQuery();
+                        sqlConnection.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                Console.WriteLine("An error occurred. " + e.Message);
+                throw;
+            }
+            return isSuccess;
+        }
+    }
+}
---
+++ ADO.NET_Code/ADO .NET Connection/Program.cs
@@ -0,0 +1,180 @@
+﻿using ADO.NetConnection.BO;
+using ADO.NetConnection.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ADO.NetConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dal = new StudentDAL();
+
+            //dal.DeleteStudent(785);
+            var studentList = new List<Student>();
+            studentList = dal.GetStudents();
+            dal.CreateStudentWithSP(new Student
+            {
+                FN = "Indranil",
+                LN = "Das",
+                RollNo = 1,
+                Marks = 104.12
+            });
+
+            var s = dal.GetStudentByRollNumber(1234);
+            var x = dal.UpdateStudent(new Student
+            {
+                FN = "Indranil",
+                LN = "Das",
+                RollNo = 1,
+                Marks = 104.12
+            });
+             x = dal.CreateStudent(new Student
+            {
+                FN = "Raj",
+                LN = "Roy",
+                RollNo = 2,
+                Marks = 104.12
+            });
+            int size = 0;
+            for (; ; )
+            {
+                Console.WriteLine("1. Enter inputs");
+                Console.WriteLine("2. Print Data");
+                Console.WriteLine("3. Find a Student by Name.");
+                Console.WriteLine("4. Find a Student by Roll.");
+                Console.WriteLine("5. Change the Last-Name of all Students");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Console.WriteLine("Number of students");
+                    
+                    var i = Convert.ToInt32(Console.ReadLine());
+                    size = i;                    
+                    
+                    if (i != 0)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            var st = new Student();
+                            Console.WriteLine($"Enter following details for Student {j+1}");
+                            Console.WriteLine("Enter first name");
+                            st.FN = Console.ReadLine();
+
+                            Console.WriteLine("Enter last name");
+                            st.LN = Console.ReadLine();
+
+                            Console.WriteLine("Enter rollno");
+                            st.RollNo = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Enter Marks");
+                            st.Marks = double.Parse(Console.ReadLine());
+
+                            studentList.Add(st);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid input");
+                    }
+                }
+                else if (input == "2")
+                {
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        Student st = studentList[i];
+                        Console.WriteLine($"Thank you for providing the details. Following are your details for student {i+1}-");
+                        Console.WriteLine($"FirstName-{st.FN}");
+                        Console.WriteLine($"LastName-{st.LN}");
+                        Console.WriteLine($"Rollnum-{st.RollNo}");
+
+                        Console.WriteLine($"Marks-{st.Marks}");
+                    }
+                   
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("Enter the First Name of the sutend you want to find.");
+                    var firstname = Console.ReadLine();
+                    var person = studentList.Where(x => x.FN == firstname).FirstOrDefault();
+                    Console.WriteLine("Latst name : "); Console.WriteLine(person.LN);
+                    Console.WriteLine("Roll Number: "); Console.WriteLine(person.RollNo);
+                }
+
+                else if (input == "4")
+                {
+                    Console.WriteLine("Enter the Roll Number of the Student you want to find");
+                    var rollNo = Convert.ToInt32(Console.ReadLine());
+                    var student1 = studentList.Where(x => x.RollNo == rollNo).FirstOrDefault(); //Find a particular student
+                    var name = student1.FN + student1.LN;
+                    Console.WriteLine(name);
+
+                }
+                else if (input == "5")
+                {
+                    Console.WriteLine("Changed the Last Name");
+                    foreach (var item in studentList) // foreach (Student item in studentlist)
+                    {
+                        // execute code
+                        item.LN = "TCS"; // lastname of every student will be changed. 
+                        var name1 = item.FN + " " + item.LN;
+                        Console.WriteLine(name1);
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                    break;
+                }
+            }
+        }
+
+        static void Main1()
+        {
+            var length = 10;
+            var i = 0;
+            for (i = 0; i < length; i++)
+            {
+                //code execution block
+            }
+
+            length = 5;
+           
+            while(i<=length)
+            {
+                //code execution block
+                i++;
+            }
+
+            do
+            {
+                i++;
+                //code execution block
+                //i++;
+            }
+            while (i <= 5);
+
+            var studentList = new List<Student>();
+
+            foreach (Student item in studentList)
+            {
+                //code execution block
+                Console.WriteLine(item.FN);
+                item.LN = "ABC";
+            }
+
+            //Find specific student
+            var rollNo =Convert.ToInt32(Console.ReadLine());
+            var fn = Console.ReadLine();
+            var student=studentList.Where(x => x.RollNo==rollNo).FirstOrDefault();
+
+            var students = studentList.Where(x => x.FN == fn).ToList();
+        }
+    }
+}
+
